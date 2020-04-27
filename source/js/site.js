fd441a17ee5baeab929d8d9b7984cd1f41005de9
Jacky Kiu <jacky@ekkko.com>
2020-04-27 11:10:23 +0800

    duo column sidebar prototype test complete

--- source/js/site.js
+++ source/js/site.js
@@ -1,11 +1,20 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  $('#sidebar-wrapper .collapse').collapse('hide');
-
+  // Menu Toggle Script
   $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
   });
+
+  // Keep it if needed
+  $(window).resize(function() {
+    if ($(window).width() > 768) {
+      $("#wrapper").removeClass("toggled");
+    };
+  });
+
+  // Sidebar menu Dropdown Script
+  $('#sidebar-secondary .collapse').collapse('hide');
 
 })(jQuery); // End of use strict
