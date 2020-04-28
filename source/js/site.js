d05e62ce06b0b74955bbacc0d88542b860798ca8
Jacky Kiu <jacky@ekkko.com>
2020-04-28 16:07:25 +0800

    update responsive setting

--- source/js/site.js
+++ source/js/site.js
@@ -9,12 +9,15 @@
 
   // Keep it if needed
   $(window).resize(function() {
-    if ($(window).width() > 768) {
+    if ($(window).width() > 992) {
       $("#wrapper").removeClass("toggled");
     };
   });
 
   // Sidebar menu Dropdown Script
   $('#sidebar-secondary .collapse').collapse('hide');
+
+  // Tooltips
+  $('[data-toggle="tooltip"]').tooltip()
 
 })(jQuery); // End of use strict
