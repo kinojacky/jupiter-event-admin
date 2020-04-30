8405d80751cf87e916b45e5ec5603f90f073c87f
Jacky Kiu <jacky@ekkko.com>
2020-04-30 16:33:31 +0800

    updated login pages content

--- source/js/site.js
+++ source/js/site.js
@@ -1,11 +1,14 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  $('.alert.logout').delay(5000).fadeTo("slow", 0.00, function(){ //fade
+  // Demo only
+  $('.alert.notice').delay(3000).fadeTo("slow", 0.00, function(){ //fade
     $(this).slideUp("slow", function() { //slide up
       $(this).remove(); //then remove from the DOM
     });
   });
+
+  $('.alert.reset-pwd').hide();
 
   // Menu Toggle Script
   $("#menu-toggle").click(function(e) {
