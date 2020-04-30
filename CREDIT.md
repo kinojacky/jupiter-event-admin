30a07b3747c68c49429bba40fdfc899cc54295a2
Jacky Kiu <jacky@ekkko.com>
2020-04-30 13:08:51 +0800

    updated login pages

---
+++ CREDIT.md
@@ -0,0 +1,19 @@
+# Credits
+
+## Code Credits
+
+- Bootstrap v4.4.1
+- jQuery v3.4.1
+
+## Image Credits
+
+- [Jupiter](https://www.flaticon.com/authors/turkkub) By turkkub (Flaticon License)
+- [Dynamic Dummy Image Generator](https://dummyimage.com/) (MIT)
+- [Unsplash](https://unsplash.com/) (CC0)
+- [Pixabay](https://pixabay.com/) [(Pixabay License)](https://pixabay.com/service/license/)
+
+## Some design inspired by
+
+- **Start Bootstrap** - David Miller's [SB Admin 2 template](https://startbootstrap.com/themes/sb-admin-2/) (MIT)
+- **FullCalendar** - Adam Shaw's [FullCalendar Standard](https://fullcalendar.io/) (MIT)
+- **DataTable** - SpryMedia Ltd.'s [DataTable](https://datatables.net/) (MIT)
--- source/js/site.js
+++ source/js/site.js
@@ -1,13 +1,20 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  $('.alert.logout').delay(5000).fadeTo("slow", 0.00, function(){ //fade
+    $(this).slideUp("slow", function() { //slide up
+      $(this).remove(); //then remove from the DOM
+    });
+  });
+
   // Menu Toggle Script
   $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
   });
 
-  // Keep it if needed
+  // Menu Toggle Responsive Toggle Remover
+  // Remove it if no needed
   $(window).resize(function() {
     if ($(window).width() > 992) {
       $("#wrapper").removeClass("toggled");
@@ -19,5 +26,17 @@
 
   // Tooltips
   $('[data-toggle="tooltip"]').tooltip()
+
+  // OTP auto-submit
+  $('#otptoken').on('keyup',function(){
+    var val = $(this).val();
+    var len = val.length;
+
+    if(len == 6){
+      console.log("Submit!")
+      // document.forms[0].submit();
+      window.location.href = "/admin/clients/index.html";
+    }
+  });
 
 })(jQuery); // End of use strict
